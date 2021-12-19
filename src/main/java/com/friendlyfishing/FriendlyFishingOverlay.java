fc8e91fe5e5039748a50968692badbbe69e9f3d0
Eric <eric.daddio@gmail.com>
2021-12-19 04:23:39 +0000

    Bias Random Sizes of Fish

--- src/main/java/com/friendlyfishing/FriendlyFishingOverlay.java
+++ src/main/java/com/friendlyfishing/FriendlyFishingOverlay.java
@@ -1,17 +1,27 @@
 package com.friendlyfishing;
 
 import com.google.inject.Inject;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
 import net.runelite.api.ItemID;
 import net.runelite.api.widgets.WidgetItem;
 import net.runelite.client.ui.FontManager;
 import net.runelite.client.ui.overlay.WidgetItemOverlay;
+
+import net.runelite.client.ui.overlay.tooltip.Tooltip;
+import net.runelite.client.ui.overlay.tooltip.TooltipManager;
 import net.runelite.client.util.ColorUtil;
+
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
 
+@Slf4j
 public class FriendlyFishingOverlay extends WidgetItemOverlay
 {
 
@@ -19,35 +29,61 @@ public class FriendlyFishingOverlay extends WidgetItemOverlay
     private final Client client;
     private final FriendlyFishingPlugin plugin;
     private final FriendlyFishingConfig config;
+    private final TooltipManager tooltipManager;
 
     @Inject
-    FriendlyFishingOverlay(Client client, FriendlyFishingPlugin plugin, FriendlyFishingConfig config)
+    FriendlyFishingOverlay(Client client, FriendlyFishingPlugin plugin, FriendlyFishingConfig config, TooltipManager tooltipManager)
     {
         this.client = client;
         this.plugin = plugin;
         this.config = config;
+        this.tooltipManager = tooltipManager;
 
         showOnInventory();
-        //showOnBank();
     }
+
 
     @Override
     public void renderItemOverlay(Graphics2D graphics, int itemId, WidgetItem widgetItem)
     {
-        if (itemId != ItemID.RAW_TROUT)
+
+        // check it's a catchable item
+        // if not then clear the slots memory
+        // we check again here because the interface is faster than ticks
+        if (!plugin.catchables.containsKey(itemId))
         {
             return;
         }
 
+        FriendlyFishingPlugin.Fish fish = plugin.catches.get(widgetItem.getIndex());
+
+        // this fish is not worth highlighting
+        if (fish == null || !fish.size.show) {
+            return;
+        }
+
         final Rectangle bounds = widgetItem.getCanvasBounds();
-        final int drawX = bounds.x;
-        final int drawY = bounds.y + bounds.height;
-        final String text = "here fishy fishy";
+        final int x = bounds.x - 3;
+        final int y = bounds.y + 15;
+        final String text = fish.size.label + "6♤";
+
         graphics.setFont(FontManager.getRunescapeSmallFont());
         graphics.setColor(Color.BLACK);
-        graphics.drawString(text, drawX + 1, drawY + 1);
-        graphics.setColor(Color.WHITE);
-        graphics.drawString(text, drawX, drawY);
+        graphics.drawString(text, x, y);
+        graphics.drawString(text, x + 2, y + 2);
+        graphics.setColor(fish.size.color);
+        graphics.drawString(text, x + 1, y + 1);
 
+
+        // mouse is not over fish
+        // done like this because detection is spotty
+        if (!widgetItem.getCanvasBounds().contains(client.getMouseCanvasPosition().getX(), client.getMouseCanvasPosition().getY()))
+        {
+            return;
+        }
+
+        // mouse is not over fish
+        // done like this because detection is spotty
+        tooltipManager.add(new Tooltip(ColorUtil.wrapWithColorTag(fish.size.tip, Color.YELLOW)));
     }
 }
--- src/main/java/com/friendlyfishing/FriendlyFishingPlugin.java
+++ src/main/java/com/friendlyfishing/FriendlyFishingPlugin.java
@@ -3,15 +3,26 @@ package com.friendlyfishing;
 import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
-import net.runelite.api.Client;
-import net.runelite.api.GameState;
+import net.runelite.api.*;
 import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.GameTick;
+import net.runelite.api.events.ItemContainerChanged;
+import net.runelite.api.widgets.Widget;
+import net.runelite.api.widgets.WidgetInfo;
+import net.runelite.api.widgets.WidgetItem;
+import net.runelite.client.callback.ClientThread;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.OverlayManager;
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
 
 @Slf4j
 @PluginDescriptor(
@@ -21,6 +32,9 @@ public class FriendlyFishingPlugin extends Plugin
 {
 	@Inject
 	private Client client;
+
+	@Inject
+	private ClientThread clientThread;
 
 	@Inject
 	private FriendlyFishingConfig config;
@@ -59,4 +73,103 @@ public class FriendlyFishingPlugin extends Plugin
 	{
 		return configManager.getConfig(FriendlyFishingConfig.class);
 	}
-}
+
+
+	// catchables
+	protected final HashMap<Integer, Catchable> catchables = new HashMap<Integer, Catchable>()
+	{{
+		put(ItemID.RAW_TROUT, new Catchable());
+		put(ItemID.RAW_SALMON, new Catchable());
+	}};
+
+	public class Catchable
+	{
+		// TODO do something with this
+	}
+
+
+	// sizes
+	// <---- bias towards left
+	private final List<Size> sizes = Arrays.asList(
+			new Size(true, "", "Nothing Special", Color.YELLOW),
+			new Size(true, "", "Nothing Special", Color.YELLOW),
+			new Size(true, "", "Nothing Special", Color.YELLOW),
+			new Size(true, "", "Nothing Special", Color.YELLOW),
+			new Size(true, "Tiny", "Disappointing", Color.YELLOW),
+			new Size(true, "", "Nothing Special", Color.YELLOW),
+			new Size(true, "Huge", "What a catch!", Color.YELLOW),
+			new Size(true, "", "Nothing Special", Color.YELLOW),
+			new Size(true, "Whopper", "Ridiculous!", Color.YELLOW),
+			new Size(true, "", "Nothing Special", Color.YELLOW),
+			new Size(true, "Rotten", "This stinks!", Color.RED),
+			new Size(true, "GIANT", "Absolute unit.", Color.CYAN),
+			new Size(true, "GIANT", "Absolute unit.", Color.CYAN)
+	);
+
+	public class Size
+	{
+		public boolean show;
+		public String label;
+		public String tip;
+		public Color color;
+
+		public Size(Boolean show, String label, String tip, Color color) {
+			this.show = show;
+			this.label = label;
+			this.tip = tip;
+			this.color = color;
+		}
+	}
+
+	// fish management
+	protected final HashMap<Integer, Fish> catches = new HashMap<Integer, Fish>();
+
+	public class Fish
+	{
+		protected int itemId;
+		protected int index;
+		protected Size size;
+
+
+		public Fish(int itemId, int index) {
+			this.itemId = itemId;
+			this.index = index;
+			int ri = (int) ( Math.abs(Math.random() - Math.random()) * (1 + (sizes.size()-1)) ); // biased random towards 0
+			this.size = sizes.get(ri);
+		}
+	}
+
+
+	@Subscribe
+	public void onItemContainerChanged(ItemContainerChanged event) {
+
+		Widget inventoryWidget = client.getWidget(WidgetInfo.INVENTORY);
+
+		Collection<WidgetItem> items = inventoryWidget.getWidgetItems();
+
+		for (WidgetItem item: items) {
+
+			int itemId = item.getId();
+
+			if (itemId < 0 || !catchables.containsKey(itemId))
+			{
+				catches.remove(item.getIndex());
+				continue;
+			}
+
+			Fish fish = new Fish(itemId, item.getIndex());   // make a new fish
+
+			if (catches.containsKey(fish.index)) {     // check we're not replacing an existing one
+				Fish existingFish = catches.get(fish.index);
+
+				if (fish.itemId != existingFish.itemId) { // it's the same, eg. Trout == Trout
+					catches.put(fish.index, fish); // else put in the new one
+				}
+
+			} else {
+				catches.put(fish.index, fish); // else put in the new one
+			}
+		}
+
+		}
+	}
